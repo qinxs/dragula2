863b94896ed8abc25c8eb68863edd6c78c54f9a5
Lea Verou <lea@verou.me>
2021-10-28 18:49:28 +0300

    Use element.classList instead of custom classes module

--- dragula.js
+++ dragula.js
@@ -2,7 +2,6 @@
 
 var emitter = require('contra/emitter');
 var crossvent = require('crossvent');
-var classes = require('./classes');
 var doc = document;
 var documentElement = doc.documentElement;
 
@@ -149,7 +148,10 @@ function dragula (initialContainers, options) {
     _offsetX = getCoord('pageX', e) - offset.left;
     _offsetY = getCoord('pageY', e) - offset.top;
 
-    classes.add(_copy || _item, 'gu-transit');
+    var inTransit = _copy || _item;
+    if (inTransit) {
+      inTransit.classList.add('gu-transit');
+    }
     renderMirrorImage();
     drag(e);
   }
@@ -309,7 +311,7 @@ function dragula (initialContainers, options) {
     ungrab();
     removeMirrorImage();
     if (item) {
-      classes.rm(item, 'gu-transit');
+      item.classList.remove('gu-transit');
     }
     if (_renderTimer) {
       clearTimeout(_renderTimer);
@@ -415,11 +417,15 @@ function dragula (initialContainers, options) {
   }
 
   function spillOver (el) {
-    classes.rm(el, 'gu-hide');
+    if (el) {
+      el.classList.remove('gu-hide');
+    }
   }
 
   function spillOut (el) {
-    if (drake.dragging) { classes.add(el, 'gu-hide'); }
+    if (el && drake.dragging) {
+      el.classList.add('gu-hide');
+    }
   }
 
   function renderMirrorImage () {
@@ -430,17 +436,19 @@ function dragula (initialContainers, options) {
     _mirror = _item.cloneNode(true);
     _mirror.style.width = getRectWidth(rect) + 'px';
     _mirror.style.height = getRectHeight(rect) + 'px';
-    classes.rm(_mirror, 'gu-transit');
-    classes.add(_mirror, 'gu-mirror');
+
+    _mirror.classList.remove('gu-transit');
+    _mirror.classList.add('gu-mirror');
+
     o.mirrorContainer.appendChild(_mirror);
     touchy(documentElement, 'add', 'mousemove', drag);
-    classes.add(o.mirrorContainer, 'gu-unselectable');
+    o.mirrorContainer.classList.add('gu-unselectable');
     drake.emit('cloned', _mirror, _item, 'mirror');
   }
 
   function removeMirrorImage () {
     if (_mirror) {
-      classes.rm(o.mirrorContainer, 'gu-unselectable');
+      o.mirrorContainer.classList.remove('gu-unselectable');
       touchy(documentElement, 'remove', 'mousemove', drag);
       getParent(_mirror).removeChild(_mirror);
       _mirror = null;
